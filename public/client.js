b33f23ca600a983c3def6533b2ffe1d9b170e023
itsNiazi <niazitareqmejl@gmail.com>
2023-05-08 14:46:06 +0200

    Added light/dark mode functionality

--- public/client.js
+++ public/client.js
@@ -1,3 +1,33 @@
+// Light & Dark-mode
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkMode", "enabled");
+};
+
+darkModeToggle.addEventListener("click", () => {});
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+// Slide & Fade-in effect
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
