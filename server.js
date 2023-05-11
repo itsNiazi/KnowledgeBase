837033e4db3735daed4987853cc508a4a7e1c0ed
itsNiazi <niazitareqmejl@gmail.com>
2023-05-11 22:59:35 +0200

    Live fuzzy search fixed with technical help

--- server.js
+++ server.js
@@ -46,43 +46,43 @@ app.use("/users", usersRoute);
 app.use("/users/dashboard/notes", notesRoute);
 app.use("/users/achievements", rewardsRoute);
 
-app.get("/search", (req, res) => {
-  const searchTerm = req.query.term;
-  const userId = req.user.id;
-
-  pool.query(
-    `SELECT * FROM notes WHERE user_id = $1 AND title ILIKE $2 ORDER BY title LIMIT 5`,
-    [userId, `%${searchTerm}%`],
-    (error, result) => {
-      if (error) {
-        console.error("Error executing search query", error);
-        res.sendStatus(500);
-      } else {
-        const notes = result.rows;
-        console.log(notes);
-        res.send(notes);
-      }
-    }
-  );
-});
-
 // app.get("/search", (req, res) => {
+//   const searchTerm = req.query.term;
 //   const userId = req.user.id;
 
 //   pool.query(
-//     `SELECT * FROM notes WHERE user_id = $1 ORDER BY title`,
-//     [userId],
+//     `SELECT * FROM notes WHERE user_id = $1 AND title ILIKE $2 ORDER BY title LIMIT 5`,
+//     [userId, `%${searchTerm}%`],
 //     (error, result) => {
 //       if (error) {
 //         console.error("Error executing search query", error);
 //         res.sendStatus(500);
 //       } else {
 //         const notes = result.rows;
+//         console.log(notes);
 //         res.send(notes);
 //       }
 //     }
 //   );
 // });
+
+app.get("/search", (req, res) => {
+  const userId = req.user.id;
+
+  pool.query(
+    `SELECT * FROM notes WHERE user_id = $1 ORDER BY title`,
+    [userId],
+    (error, result) => {
+      if (error) {
+        console.error("Error executing search query", error);
+        res.sendStatus(500);
+      } else {
+        const notes = result.rows;
+        res.send(notes);
+      }
+    }
+  );
+});
 
 // 404 Page Not Found
 app.get("*", (req, res) => {
