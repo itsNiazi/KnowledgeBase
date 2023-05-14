6a4c88f8e7945a38f14495455468d65c612c69fb
Naderr <naderking1000@gmail.com>
2023-05-15 00:46:06 +0200

    Fixed theme toggler
    Har gjort om hela funktionen då den inte var optimal. Nu kan man byta mellan färgteman utan att behöva inaktivera den nuvarande man har

--- public/client.js
+++ public/client.js
@@ -1,31 +1,3 @@
-// Light & Dark-mode
-let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector("#dark-mode-toggle");
-
-const enableDarkMode = () => {
-  document.body.classList.add("darkmode");
-  localStorage.setItem("darkMode", "enabled");
-};
-
-const disableDarkMode = () => {
-  document.body.classList.remove("darkmode");
-  localStorage.setItem("darkMode", null);
-};
-
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
-
-darkModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  darkMode = localStorage.getItem("darkMode");
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
-});
-
 // Slide & Fade-in effect
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -40,144 +12,40 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenSections = document.querySelectorAll(".hidden");
 hiddenSections.forEach((el) => observer.observe(el));
 
-// Red-theme//
-let redMode = localStorage.getItem("redMode");
-const redModeToggle = document.querySelector("#red-mode-toggle");
+// theme-toggler//
+const colorButtons = document.querySelectorAll(".color-button");
+const body = document.body;
 
-const enableRedMode = () => {
-  document.body.classList.add("redmode");
-  localStorage.setItem("redMode", "enabled");
-};
-
-const disableRedMode = () => {
-  document.body.classList.remove("redmode");
-  localStorage.setItem("redMode", null);
-};
-
-if (redMode === "enabled") {
-  enableRedMode();
-}
-
-redModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  redMode = localStorage.getItem("redMode");
-  if (redMode !== "enabled") {
-    enableRedMode();
-  } else {
-    disableRedMode();
-  }
+colorButtons.forEach(function(button, index) {
+  button.style.backgroundColor = `var(--color-${index + 1})`;
+  button.addEventListener("click", function() {
+    const color = getComputedStyle(button).getPropertyValue("background-color");
+    const textColor = getComputedStyle(button).getPropertyValue("color");
+    body.style.transition = "background-color 0.5s ease-in-out";
+    body.style.backgroundColor = color;
+    body.style.color = textColor;
+    localStorage.setItem("theme-color", color);
+    localStorage.setItem("text-color", textColor);
+    colorButtons.forEach(function(button) {
+      if (getComputedStyle(button).getPropertyValue("background-color") === color) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  });
 });
 
-//orange-theme//
-
-let orangeMode = localStorage.getItem("orangeMode");
-const orangeModeToggle = document.querySelector("#orange-mode-toggle");
-
-const enableOrangeMode = () => {
-  document.body.classList.add("orangemode");
-  localStorage.setItem("orangeMode", "enabled");
-};
-
-const disableOrangeMode = () => {
-  document.body.classList.remove("orangemode");
-  localStorage.setItem("orangeMode", null);
-};
-
-if (orangeMode === "enabled") {
-  enableOrangeMode();
+if (localStorage.getItem("theme-color")) {
+  const color = localStorage.getItem("theme-color");
+  const textColor = localStorage.getItem("text-color");
+  body.style.backgroundColor = color;
+  body.style.color = textColor;
+  colorButtons.forEach(function(button) {
+    if (getComputedStyle(button).getPropertyValue("background-color") === color) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 }
-
-orangeModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  orangeMode = localStorage.getItem("orangeMode");
-  if (orangeMode !== "enabled") {
-    enableOrangeMode();
-  } else {
-    disableOrangeMode();
-  }
-});
-
-//green-theme//
-
-let greenMode = localStorage.getItem("greenMode");
-const greenModeToggle = document.querySelector("#green-mode-toggle");
-
-const enableGreenMode = () => {
-  document.body.classList.add("greenmode");
-  localStorage.setItem("greenMode", "enabled");
-};
-
-const disableGreenMode = () => {
-  document.body.classList.remove("greenmode");
-  localStorage.setItem("greenMode", null);
-};
-
-if (greenMode === "enabled") {
-  enableGreenMode();
-}
-
-greenModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  greenMode = localStorage.getItem("greenMode");
-  if (greenMode !== "enabled") {
-    enableGreenMode();
-  } else {
-    disableGreenMode();
-  }
-});
-
-//blue-theme//
-let blueMode = localStorage.getItem("blueMode");
-const blueModeToggle = document.querySelector("#blue-mode-toggle");
-
-const enableBlueMode = () => {
-  document.body.classList.add("bluemode");
-  localStorage.setItem("blueMode", "enabled");
-};
-
-const disableBlueMode = () => {
-  document.body.classList.remove("bluemode");
-  localStorage.setItem("blueMode", null);
-};
-
-if (blueMode === "enabled") {
-  enableBlueMode();
-}
-
-blueModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  blueMode = localStorage.getItem("blueMode");
-  if (blueMode !== "enabled") {
-    enableBlueMode();
-  } else {
-    disableBlueMode();
-  }
-});
-
-//purple-theme//
-let purpleMode = localStorage.getItem("purpleMode");
-const purpleModeToggle = document.querySelector("#purple-mode-toggle");
-
-const enablePurpleMode = () => {
-  document.body.classList.add("purplemode");
-  localStorage.setItem("purpleMode", "enabled");
-};
-
-const disablePurpleMode = () => {
-  document.body.classList.remove("purplemode");
-  localStorage.setItem("purpleMode", null);
-};
-
-if (purpleMode === "enabled") {
-  enablePurpleMode();
-}
-
-purpleModeToggle.addEventListener("click", () => {
-  console.log("Click!");
-  purpleMode = localStorage.getItem("purpleMode");
-  if (purpleMode !== "enabled") {
-    enablePurpleMode();
-  } else {
-    disablePurpleMode();
-  }
-});
