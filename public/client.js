58e44c5d729372ed66101b318636eb8334fec3d5
Naderr <naderking1000@gmail.com>
2023-05-15 19:37:44 +0200

    Changed storing method
    Originally we had stored preferences in localStorage but now I have changed it so it saves preferences using cookies

--- public/client.js
+++ public/client.js
@@ -16,6 +16,27 @@ hiddenSections.forEach((el) => observer.observe(el));
 const colorButtons = document.querySelectorAll(".color-button");
 const body = document.body;
 
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/`;
+}
+
+function getCookie(name) {
+  const cookieName = `${name}=`;
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
+
 colorButtons.forEach(function(button, index) {
   button.style.backgroundColor = `var(--color-${index + 1})`;
   button.addEventListener("click", function() {
@@ -24,8 +45,8 @@ colorButtons.forEach(function(button, index) {
     body.style.transition = "background-color 0.5s ease-in-out";
     body.style.backgroundColor = color;
     body.style.color = textColor;
-    localStorage.setItem("theme-color", color);
-    localStorage.setItem("text-color", textColor);
+    setCookie("theme-color", color, 30); // Save color preference for 30 days
+    setCookie("text-color", textColor, 30); // Save text color preference for 30 days
     colorButtons.forEach(function(button) {
       if (getComputedStyle(button).getPropertyValue("background-color") === color) {
         button.classList.add("active");
@@ -36,13 +57,14 @@ colorButtons.forEach(function(button, index) {
   });
 });
 
-if (localStorage.getItem("theme-color")) {
-  const color = localStorage.getItem("theme-color");
-  const textColor = localStorage.getItem("text-color");
-  body.style.backgroundColor = color;
+const themeColor = getCookie("theme-color");
+const textColor = getCookie("text-color");
+
+if (themeColor && textColor) {
+  body.style.backgroundColor = themeColor;
   body.style.color = textColor;
   colorButtons.forEach(function(button) {
-    if (getComputedStyle(button).getPropertyValue("background-color") === color) {
+    if (getComputedStyle(button).getPropertyValue("background-color") === themeColor) {
       button.classList.add("active");
     } else {
       button.classList.remove("active");
