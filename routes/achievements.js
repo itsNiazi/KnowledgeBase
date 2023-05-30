68078c8e1d58f5dab07b561747863b5f5fa90f61
onv2487 <onvsda2486@gmail.com>
2023-05-31 00:12:26 +0200

    Fixed progress-bar, home.ejs and css.

--- routes/achievements.js
+++ routes/achievements.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const achievementsController = require("../controllers/achievementsController");
-const usersController = require("../controllers/usersController");
 const { checkNotAuthenticated } = require("../controllers/authController");
 // const { use } = require("passport");
 
