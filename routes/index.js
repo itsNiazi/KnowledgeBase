d7dcc387535595b8146fea813afc7b9a7077df8a
onv2487 <onvsda2486@gmail.com>
2023-05-31 00:30:03 +0200

    Unnecessary code

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
-const achievementsController = require("../controllers/achievementsController");
 
 // Index page route
-router.get("/", indexController.getIndex, achievementsController.getUserAchievements);
+router.get("/", indexController.getIndex);
 
 module.exports = router;
